de46fb073a1160e93ee42390c6d3bf77852b655f
jmtr1 <87749945+jmtr1@users.noreply.github.com>
2026-04-30 14:08:59 +0200

    final

--- initialise.sh
+++ initialise.sh
@@ -35,46 +35,46 @@ EOF
 
 # Install system dependencies and fonts
 
-# sudo rm -f /etc/apt/sources.list.d/*ubuntugis*
+sudo rm -f /etc/apt/sources.list.d/*ubuntugis*
 
-# sudo apt clean
+sudo apt clean
 
-# sudo apt update -y
+sudo apt update -y
 
-# sudo apt install -y ghostscript libreoffice xvfb fontconfig fonts-liberation2 texlive-latex-extra texlive-pictures
+sudo apt install -y ghostscript libreoffice xvfb fontconfig fonts-liberation2 texlive-latex-extra texlive-pictures
 
-# sudo tlmgr install tikz-3dplot
+sudo tlmgr install tikz-3dplot
 
-# # Copy Arial Narrow fonts if available
+# Copy Arial Narrow fonts if available
 
-# mkdir -p "$HOME/.local/share/fonts"
+mkdir -p "$HOME/.local/share/fonts"
 
-# find "/home/onyxia/work/ai-patents-and-innovation/fonts" -maxdepth 1 -type f \
+find "/home/onyxia/work/ai-patents-and-innovation/fonts" -maxdepth 1 -type f \
 
-#     \( -iname 'arial-narrow*.ttf' -o -iname 'arialnarrow*.ttf' -o -iname 'arial narrow*.ttf' \
+    \( -iname 'arial-narrow*.ttf' -o -iname 'arialnarrow*.ttf' -o -iname 'arial narrow*.ttf' \
 
-#     -o -iname 'ArialNarrow*.ttf' -o -iname 'Arial Narrow*.ttf' \) \
+    -o -iname 'ArialNarrow*.ttf' -o -iname 'Arial Narrow*.ttf' \) \
 
-#     -print0 | xargs -0 -r -I{} cp -f "{}" "$HOME/.local/share/fonts/"
+    -print0 | xargs -0 -r -I{} cp -f "{}" "$HOME/.local/share/fonts/"
 
-# fc-cache -f -v
+fc-cache -f -v
 
-# # Install Material Icon Theme extension
+# Install Material Icon Theme extension
 
-# wget --retry-on-http-error=429 https://github.com/jmtr1/OECD-onyxia-workspace/raw/refs/heads/main/pkief.material-icon-theme-5.27.0.vsix -O material-icon-theme.vsix
+wget --retry-on-http-error=429 https://github.com/jmtr1/OECD-onyxia-workspace/raw/refs/heads/main/pkief.material-icon-theme-5.27.0.vsix -O material-icon-theme.vsix
 
-# code-server --install-extension "$(pwd)/material-icon-theme.vsix"
+code-server --install-extension "$(pwd)/material-icon-theme.vsix"
 
-# wget --retry-on-http-error=429 https://github.com/jmtr1/OECD-onyxia-workspace/raw/refs/heads/main/jupyterlab-light-theme.vsix -O jupyterlab-light-theme.vsix
+wget --retry-on-http-error=429 https://github.com/jmtr1/OECD-onyxia-workspace/raw/refs/heads/main/jupyterlab-light-theme.vsix -O jupyterlab-light-theme.vsix
 
-# code-server --install-extension "$(pwd)/jupyterlab-light-theme.vsix"
+code-server --install-extension "$(pwd)/jupyterlab-light-theme.vsix"
 
-# code-server --install-extension mathematic.vscode-pdf
+code-server --install-extension mathematic.vscode-pdf
 
-# # Install Python packages (commented for now) NEW!
+# Install Python packages (commented for now) NEW!
 
-# uv pip install --system torch dask transformers ipywidgets boto3 openai dotenv optuna lightgbm wandb igraph openpyxl nbconvert botocore==1.40.18 s3fs matplotlib-venn
+uv pip install --system torch dask transformers ipywidgets boto3 openai dotenv optuna lightgbm wandb igraph openpyxl nbconvert botocore==1.40.18 s3fs matplotlib-venn
 
-# rm -rf inkscape.appimage material-icon-theme.vsix jupyterlab-light-theme.vsix
+rm -rf inkscape.appimage material-icon-theme.vsix jupyterlab-light-theme.vsix
 
-# mkdir -p "/home/onyxia/work/ai-patents-and-innovation/output/figures"
+mkdir -p "/home/onyxia/work/ai-patents-and-innovation/output/figures"
